c0a09598a9ed978419bf49a5f7c6f756ddf1e318
Michael Tan <michaeltan@MacBook-Pro.fios-router.home>
2021-12-03 17:44:47 -0500

    started adding toggle

--- project2/src/components/Navbar.jsx
+++ project2/src/components/Navbar.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
-// import { useState } from 'react'
+import { useState } from 'react'
 
 
 export default function Navbar() {
 
-  // const [navbarOpen, setNavbarOpen] = useState(false)
+  const [navbarOpen, setNavbarOpen] = useState(false)
 
-  // const handleToggle = () => {
-  //   setNavbarOpen(prev => !prev)
-  // }
+  const handleToggle = () => {
+    setNavbarOpen(prev => !prev)
+  }
 
   return (
     <div>
+      <div class="container" onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}>
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </div>
       <nav>
         <div className='nav-container'>
-        {/* <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button> */}
           <Link to='/'>Home</Link>
           <Link to='/About'>About</Link>
           <Link to='/TellMe'>Tell Me</Link>
